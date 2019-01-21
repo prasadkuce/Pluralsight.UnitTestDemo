02a0c3322fba4fc4b5dad05471d1693388e95b27
prasad.gs <prasad.gs@HYD2NB0010>
2019-01-21 07:04:06 +0530

    Subtract

--- src/Pluralsight.UnitTestDemo.Api/Calculator.cs
+++ src/Pluralsight.UnitTestDemo.Api/Calculator.cs
@@ -8,5 +8,9 @@ namespace Pluralsight.UnitTestDemo.Api
         {
             return val1 + val2;
         }
+        public int Subtract(int val1, int val2)
+        {
+            return val1 - val2;
+        }
     }
 }
--- test/Pluralsight.UnitTestDemo.Tests/CalculatorFixture.cs
+++ test/Pluralsight.UnitTestDemo.Tests/CalculatorFixture.cs
@@ -4,7 +4,7 @@ using Pluralsight.UnitTestDemo.Api;
 namespace Pluralsight.UnitTestDemo.Tests
 {
     [TestClass]
-    public class CalculateFixtre
+    public class CalculateFixture
     {
         [TestMethod]
         public void Add()
@@ -18,6 +18,19 @@ namespace Pluralsight.UnitTestDemo.Tests
             var result = sut.Add(val1, val2);
             //assert
             Assert.AreEqual<int>(expected,result,"Add Failed");
+        }
+        [TestMethod]
+        public void Subtract()
+        {
+            //arrange
+            var val1 = 1;
+            var val2 = 2;
+            var expected = 3;
+            //act
+            var sut = new Calculator();
+            var result = sut.Subtract(val1, val2);
+            //assert
+            Assert.AreEqual<int>(expected, result, "Add Failed");
         }
     }
 }
